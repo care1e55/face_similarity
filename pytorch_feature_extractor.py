dcd2558b8e745931f8df2c744cbccfc919f99372
care1e55 <sfilms89@gmail.com>
2021-01-13 18:21:04 +0300

    0.75

--- pytorch_feature_extractor.py
+++ pytorch_feature_extractor.py
@@ -8,7 +8,9 @@ from PIL import Image
 from tqdm import tqdm
 from torchvision import transforms
 from facenet_pytorch import MTCNN
-import model.resnet50_128 as model
+# import model.resnet50_128 as model
+# import model.resnet50_2048 as model
+import model.senet50_2048 as model
 from facenet_pytorch import MTCNN
 # hyper parameters
 batch_size = 16
@@ -63,24 +65,38 @@ def chunks(l, n):
 
 def initialize_model():
     # Download the pytorch model and weights.
-    network = model.resnet50_128(weights_path='./model/resnet50_128.pth').to(DEVICE)
+    # network = model.resnet50_128(weights_path='./model/resnet50_128.pth').to(DEVICE)
+    # network = model.resnet50_ft(weights_path='./model/resnet50_2048.pth').to(DEVICE)
+    network = model.senet50_ft(weights_path='./model/senet50_2048.pth').to(DEVICE)
     network.eval()
     return network
 
 
 def image_encoding(model, facepaths):
     num_faces = len(facepaths)
-    face_feats = np.empty((num_faces, 128))
+    # face_feats = np.empty((num_faces, 128))
+    face_feats = np.empty((num_faces, 2048))
     imgpaths = facepaths
     imgchunks = list(chunks(imgpaths, batch_size))
 
     for c, imgs in tqdm(enumerate(imgchunks), total=len(imgchunks)):
         im_array = np.array([load_data(path=i, shape=(224, 224, 3)) for i in imgs])
-        f = model(torch.Tensor(im_array.transpose(0, 3, 1, 2)).to(DEVICE))[1].detach().cpu().numpy()[:, :, 0, 0]
+        # torch.Size([16, 128])
+        # torch.Size([16, 128, 1, 1])
+        # print()
+        # print(model(torch.Tensor(im_array.transpose(0, 3, 1, 2)).to(DEVICE))[0].size())
+        # print(model(torch.Tensor(im_array.transpose(0, 3, 1, 2)).to(DEVICE))[1].size())
+        # print(model(torch.Tensor(im_array.transpose(0, 3, 1, 2)).to(DEVICE)).size())
+
+        # f = model(torch.Tensor(im_array.transpose(0, 3, 1, 2)).to(DEVICE))[1].detach().cpu().numpy()[:, :, 0, 0]
+        # print(f.shape)
+        f = model(torch.Tensor(im_array.transpose(0, 3, 1, 2)).to(DEVICE)).detach().cpu().numpy()[:, :, 0, 0]
+        # print(f.shape)
         start = c * batch_size
         end = min((c + 1) * batch_size, num_faces)
         # This is different from the Keras model where the normalization has been done inside the model.
         face_feats[start:end] = f / np.sqrt(np.sum(f ** 2, -1, keepdims=True))
+    # print(face_feats.shape)
     return face_feats
 
 
